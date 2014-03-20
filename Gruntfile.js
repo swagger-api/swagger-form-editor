5be206157cc119e9439c9e6c07604fa23227e8c4
Vlad Yazhbin <vlad@users.noreply.github.com>
2014-03-19 21:46:29 -0700

    disable jshint validation for now -- swagger.js raises errors

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,7 +29,7 @@ module.exports = function (grunt) {
     watch: {
       js: {
         files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
-        tasks: ['newer:jshint:all'],
+//        tasks: ['newer:jshint:all'],
         options: {
           livereload: true
         }
