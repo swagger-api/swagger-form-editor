89cb95703803964846d07a8e69cdfaa222b38437
Vlad Yazhbin <vlad@users.noreply.github.com>
2014-03-20 00:35:54 -0700

    User sees real resource and model list on left-hand side

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,10 +2,16 @@
 
 app.controller('MainCtrl', function ($scope, $http, $filter) {
   $scope.items = ['/path1', '/path2'];
+  $scope.file = null;
   $scope.fileContents = "";
   $http.get('/data/pet-data.json').success(function(data) {
-    $scope.fileContents = $filter('json')(data);
+    $scope.file = data;
+    fileObjectChanged();
   });
+
+  var fileObjectChanged = function() {
+    $scope.fileContents = $filter('json')($scope.file);
+  };
 
 //  window.swagger = new SwaggerApi({
 //    url: "http://petstore.swagger.wordnik.com/api/api-docs.json",
