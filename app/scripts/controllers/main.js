b6d33ef9d2909a90916fc462c98e179dc97cf102
Vlad Yazhbin <vlad@users.noreply.github.com>
2014-03-21 01:08:05 -0700

    editable path names and model id's

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-app.controller('MainCtrl', function ($scope, $http, $filter) {
+app.controller('MainCtrl', function ($scope, $http, $filter, $timeout) {
   $scope.items = ['/path1', '/path2'];
   $scope.file = null;
   $scope.fileContents = "";
@@ -9,19 +9,27 @@ app.controller('MainCtrl', function ($scope, $http, $filter) {
     fileObjectChanged();
   });
 
+  //listen for changes from ng-model in the rows on the left panel of the view
+  $scope.$watch('file', function(newValue, oldValue) {
+    fileObjectChanged();
+  }, true);
+
   var fileObjectChanged = function() {
     $scope.fileContents = $filter('json')($scope.file);
   };
 
-//  window.swagger = new SwaggerApi({
-//    url: "http://petstore.swagger.wordnik.com/api/api-docs.json",
-//    success: function() {
-//      if(swagger.ready === true) {
-//        // upon connect, fetch a pet and set contents to element "mydata"
-//        swagger.apis.pet.getPetById({petId:1}, function(data) {
-//          console.log(data.content.data);
-//        });
+//  $timeout(function() {
+//    window.swagger = new SwaggerApi({
+//      url: "http://petstore.swagger.wordnik.com/api/api-docs",
+//      success: function () {
+//        if (swagger.ready === true) {
+//          // upon connect, fetch a pet and set contents to element "mydata"
+////          swagger.apis.pet.getPetById({petId: 1}, function (data) {
+////            console.log(data.content.data);
+////          });
+//          console.log(swagger);
+//        }
 //      }
-//    }
-//  });
+//    });
+//  }, 1000);
 });
